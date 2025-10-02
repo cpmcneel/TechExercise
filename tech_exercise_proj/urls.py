b8eb1e7921a725f39a499878453ea91a758945d4
Ubuntu <ubuntu@ip-172-31-29-182.us-east-2.compute.internal>
2025-10-02 02:01:18 +0000

    Created hello world page for dev environment check

--- tech_exercise_proj/urls.py
+++ tech_exercise_proj/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from hello_test.views import hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', hello), #dev env check
 ]
